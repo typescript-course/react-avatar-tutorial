49f59b3472f8537b2f5eb3c832a91a37d8984bb0
Joe Previte <jjprevite@gmail.com>
2022-10-27 15:54:26 +0000

    feat: make AvatarProps optional
    This makes the AvatarProps optional and adds a fallback URL and alt
    text.

--- src/Avatar.test.tsx
+++ src/Avatar.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 
-import Avatar from "./Avatar";
+import Avatar, { FALLBACK_AVATAR_URL, FALLBACK_AVATAR_ALT_TEXT } from "./Avatar";
 
 describe("Avatar", () => {
     it("should render an <img />", () => {
@@ -19,5 +19,11 @@ describe("Avatar", () => {
     
         const img = screen.getByAltText(alt);
         expect(img).toHaveAttribute("src", url);
+    });
+    it("should use a fallback image if no URL passed in", () => {
+        render(<Avatar />);
+    
+        const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+        expect(img).toHaveAttribute("src", FALLBACK_AVATAR_URL);
     });
 });
